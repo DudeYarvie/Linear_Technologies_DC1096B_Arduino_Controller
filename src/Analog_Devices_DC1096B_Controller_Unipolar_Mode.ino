688c8d53b76bdd4c3c27bce283c80e09c395334a
Jarvis Hill <hilljarvis@hotmail.com>
2018-06-06 17:30:58 -0500

    Update Analog_Devices_DC1096B_Controller_Unipolar_Mode.ino

--- src/Analog_Devices_DC1096B_Controller_Unipolar_Mode.ino
+++ src/Analog_Devices_DC1096B_Controller_Unipolar_Mode.ino
@@ -2,10 +2,10 @@
           Analog_Devices_DC1096B_Controller_Unipolar_Mode.ino 
 Author: Jarvis Hill (hilljarvis@gmail.com)
 Date: 06-JUNE-2018
-Purpose: Use an Arduino UNO (Atmega328) to control and drive the oout of the Analog Devices DC1096B demo board set for unipolar output configuration (0V to +5V).
+Purpose: Use an Arduino UNO (Atmega328) to control and drive the output of the Analog Devices DC1096B demo board set for unipolar output configuration (0V to +5V).
 
 Usage:
-         1. Connect DC1096B demo board to Arduino UNO per the connection diagram in sketch folder.
+         1. Connect DC1096B demo board to Arduino UNO per the connection diagram.
                           
 References: DC1096B user manaul, LTC2642ACDD-16 16-bit DAC datasheet
 */
